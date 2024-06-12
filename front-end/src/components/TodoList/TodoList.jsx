a73dca6cad7d63f10c67f29b288b8cb4827d59b9
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2024-06-12 19:05:55 -0300

    Adiciona estilo dinâmico ao completar todo e botão para completar todo

--- front-end/src/components/TodoList/TodoList.jsx
+++ front-end/src/components/TodoList/TodoList.jsx
@@ -1,11 +1,15 @@
 import stylesButton from "../../styles/button.module.css";
 import styles from "./TodoList.module.css";
 
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { clsx } from "clsx";
 
-export const TodoList = ({ todos, onClickDeleteTodo }) => {
+export const TodoList = ({ todos, onClickDeleteTodo, onClickCompleteTodo }) => {
+  const handleClickCompleteTodo = (id) => {
+    onClickCompleteTodo(id);
+  };
+
   const handleClickDeleteTodo = (id) => {
     onClickDeleteTodo(id);
   };
@@ -17,10 +21,24 @@ export const TodoList = ({ todos, onClickDeleteTodo }) => {
           key={todo.id}
           className={clsx(
             styles["todo-list__item"],
-            styles["todo-list__item--completed"]
+            todo.completed && styles["todo-list__item--completed"]
           )}
         >
-          <span>{todo.description}</span>
+          <span className={styles["todo-list__description"]}>
+            {todo.description}
+          </span>
+          <button
+            className={clsx(
+              stylesButton["button"],
+              stylesButton["button--primary"],
+              stylesButton["button--icon"],
+              styles["todo-list__button"]
+            )}
+            disabled={todo.completed}
+            onClick={() => handleClickCompleteTodo(todo.id)}
+          >
+            <FontAwesomeIcon icon={faCheck} />
+          </button>
           <button
             className={clsx(
               stylesButton["button"],
