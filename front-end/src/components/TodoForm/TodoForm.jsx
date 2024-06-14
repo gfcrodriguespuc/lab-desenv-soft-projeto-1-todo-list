9f7267d1e249e04f59cf58a5224926df423a4209
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2024-06-14 20:43:32 -0300

    Implementa criação de todos de diferentes tipos e prioridades

--- front-end/src/components/TodoForm/TodoForm.jsx
+++ front-end/src/components/TodoForm/TodoForm.jsx
@@ -6,9 +6,24 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { clsx } from "clsx";
 import { useState } from "react";
+import { TodoPriority, TodoType } from "../../models/todo";
 
 export const TodoForm = ({ addTodo }) => {
   const [todoDescription, setTodoDescription] = useState("");
+  const [todoType, setTodoType] = useState(TodoType.FREE);
+  const [todoPriority, setTodoPriority] = useState(TodoPriority.LOW);
+  const [todoDueDate, setTodoDueDate] = useState("");
+  const [todoDeadline, setTodoDeadline] = useState("");
+
+  const handleTypeChange = (event) => {
+    console.debug("handleTypeChange", event.target.value);
+    setTodoType(event.target.value);
+  };
+
+  const handlePriorityChange = (event) => {
+    console.debug("handlePriorityChange", event.target.value);
+    setTodoPriority(event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -18,33 +33,143 @@ export const TodoForm = ({ addTodo }) => {
       return;
     }
 
-    addTodo(todoDescriptionTrim);
+    addTodo({
+      type: todoType,
+      priority: todoPriority,
+      dueDate: todoType === TodoType.DUE_DATE ? todoDueDate : null,
+      deadline: todoType === TodoType.DEADLINE ? +todoDeadline : null,
+      description: todoDescriptionTrim,
+    });
     setTodoDescription("");
   };
 
   return (
     <form className={styles["todo-form"]} onSubmit={handleSubmit}>
-      <input
-        className={clsx(
-          inputStyles["input-text"],
-          inputStyles["input-text--primary"],
-          inputStyles["input-text--full"]
+      {/*
+        Tipos possiveis para a tarefa:
+        A ToDo List deverá possuir três tipos de tarefas:
+          o 1. Data (com data prevista de conclusão)
+          o 2. Prazo (com prazo previsto de conclusão informado em dias)
+          o 3. Livre (Sem data ou prazo previsto de conclusão)
+        As tarefas deverão ter três níveis de prioridades:
+          o 1. Alta
+          o 2. Média
+          o 3. Baixa
+      */}
+      <div>
+        Tipo de tarefa:
+        <label>
+          Data prevista
+          <input
+            type="radio"
+            value={TodoType.DUE_DATE}
+            onChange={handleTypeChange}
+            checked={todoType === TodoType.DUE_DATE}
+          />
+        </label>
+        <label>
+          Prazo previsto
+          <input
+            type="radio"
+            value={TodoType.DEADLINE}
+            onChange={handleTypeChange}
+            checked={todoType === TodoType.DEADLINE}
+          />
+        </label>
+        <label>
+          Livre
+          <input
+            type="radio"
+            value={TodoType.FREE}
+            onChange={handleTypeChange}
+            checked={todoType === TodoType.FREE}
+          />
+        </label>
+      </div>
+
+      <div>
+        {todoType === TodoType.DUE_DATE && (
+          <input
+            className={clsx(
+              inputStyles["input-text"],
+              inputStyles["input-text--primary"]
+            )}
+            type="date"
+            min={new Date().toISOString().split("T")[0]}
+            value={todoDueDate}
+            onChange={(event) => setTodoDueDate(event.target.value)}
+          />
         )}
-        type="text"
-        placeholder="Qual sua nova tarefa?"
-        value={todoDescription}
-        onChange={(event) => setTodoDescription(event.target.value)}
-      />
-      <button
-        className={clsx(
-          buttonStyles["button"],
-          buttonStyles["button--primary"],
-          buttonStyles["button--icon"]
+
+        {todoType === TodoType.DEADLINE && (
+          <input
+            className={clsx(
+              inputStyles["input-text"],
+              inputStyles["input-text--primary"]
+            )}
+            type="number"
+            min="1"
+            placeholder="Prazo em dias"
+            value={todoDeadline}
+            onChange={(event) => setTodoDeadline(event.target.value)}
+          />
         )}
-        type="submit"
-      >
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
+      </div>
+
+      <div>
+        Prioridade:
+        <label>
+          Baixa
+          <input
+            type="radio"
+            value={TodoPriority.LOW}
+            onChange={handlePriorityChange}
+            checked={todoPriority === TodoPriority.LOW}
+          />
+        </label>
+        <label>
+          Média
+          <input
+            type="radio"
+            value={TodoPriority.MEDIUM}
+            onChange={handlePriorityChange}
+            checked={todoPriority === TodoPriority.MEDIUM}
+          />
+        </label>
+        <label>
+          Alta
+          <input
+            type="radio"
+            value={TodoPriority.HIGH}
+            onChange={handlePriorityChange}
+            checked={todoPriority === TodoPriority.HIGH}
+          />
+        </label>
+      </div>
+
+      <div>
+        <input
+          className={clsx(
+            inputStyles["input-text"],
+            inputStyles["input-text--primary"],
+            inputStyles["input-text--full"]
+          )}
+          type="text"
+          placeholder="Qual sua nova tarefa?"
+          value={todoDescription}
+          onChange={(event) => setTodoDescription(event.target.value)}
+        />
+        <button
+          className={clsx(
+            buttonStyles["button"],
+            buttonStyles["button--primary"],
+            buttonStyles["button--icon"]
+          )}
+          type="submit"
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
     </form>
   );
 };
---
+++ front-end/src/models/todo.js
@@ -0,0 +1,11 @@
+export const TodoType = Object.freeze({
+  DUE_DATE: "DUE_DATE",
+  DEADLINE: "DEADLINE",
+  FREE: "FREE",
+});
+
+export const TodoPriority = Object.freeze({
+  LOW: "LOW",
+  MEDIUM: "MEDIUM",
+  HIGH: "HIGH",
+});
