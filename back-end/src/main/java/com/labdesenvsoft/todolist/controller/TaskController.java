292490e6a916fd3c9462213ff7f712e211a60650
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2024-06-15 17:51:26 -0300

    Configura suporte OpenAPI3 e Swagger na API

--- back-end/src/main/java/com/labdesenvsoft/todolist/controller/TaskController.java
+++ back-end/src/main/java/com/labdesenvsoft/todolist/controller/TaskController.java
@@ -3,9 +3,13 @@ package com.labdesenvsoft.todolist.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class TaskController {
+
     @GetMapping("/tasks")
+    @Operation(summary = "Busca todas as tarefas da lista de tarefas")
     public String getAllTasks() {
         return "All tasks";
     }
