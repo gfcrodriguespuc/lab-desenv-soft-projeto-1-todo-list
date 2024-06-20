7745ea777dd167d01afcb20f9f58389621c997cf
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2024-06-19 22:06:00 -0300

    Move lógicas de dominio para o TaskService

--- back-end/src/main/java/com/labdesenvsoft/todolist/controller/TaskController.java
+++ back-end/src/main/java/com/labdesenvsoft/todolist/controller/TaskController.java
@@ -1,7 +1,6 @@
 package com.labdesenvsoft.todolist.controller;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +15,8 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.labdesenvsoft.todolist.domain.entity.Task;
-import com.labdesenvsoft.todolist.repository.ITaskRepository;
+import com.labdesenvsoft.todolist.domain.exception.TaskNotFoundException;
+import com.labdesenvsoft.todolist.service.TaskService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
@@ -26,15 +26,14 @@ import lombok.extern.slf4j.Slf4j;
 public class TaskController {
 
     @Autowired
-    ITaskRepository taskRepository;
+    TaskService taskService;
 
     @GetMapping("/tasks")
     @Operation(summary = "Busca todas as tarefas da lista de tarefas")
-    public ResponseEntity<List<Task>> getAllTasks() {
+    public ResponseEntity<Iterable<Task>> getAllTasks() {
         try {
 
-            List<Task> tasks = new LinkedList<>();
-            taskRepository.findAll().forEach(tasks::add);
+            Collection<Task> tasks = taskService.getAllTasks();
 
             if (tasks.isEmpty()) {
                 return ResponseEntity.noContent().build();
@@ -51,11 +50,11 @@ public class TaskController {
     @Operation(summary = "Cria uma nova tarefa")
     public ResponseEntity<Void> postTask(@RequestBody Task taskToCreate, UriComponentsBuilder uriBuilder) {
         try {
-            Task task = taskRepository.save(taskToCreate);
+            long taskId = taskService.createTask(taskToCreate);
 
             UriComponents uriComponents = uriBuilder
                     .path("/tasks/{id}")
-                    .buildAndExpand(task.getId());
+                    .buildAndExpand(taskId);
 
             return ResponseEntity.created(uriComponents.toUri()).build();
         } catch (Exception e) {
@@ -68,13 +67,10 @@ public class TaskController {
     @Operation(summary = "Busca uma tarefa específica")
     public ResponseEntity<Task> getTask(@PathVariable Long id) {
         try {
-            Task task = taskRepository.findById(id).orElse(null);
-
-            if (task == null) {
-                return ResponseEntity.notFound().build();
-            }
-
+            Task task = taskService.getTaskById(id);
             return ResponseEntity.ok(task);
+        } catch (TaskNotFoundException e) {
+            return ResponseEntity.notFound().build();
         } catch (Exception e) {
             log.error("Erro ao buscar uma tarefa específica", e);
             return ResponseEntity.internalServerError().build();
@@ -85,18 +81,10 @@ public class TaskController {
     @Operation(summary = "Atualiza uma tarefa existente")
     public ResponseEntity<Void> putTask(@PathVariable Long id, @RequestBody Task task) {
         try {
-            Task taskToUpdate = taskRepository.findById(id).orElse(null);
-
-            if (taskToUpdate == null) {
-                return ResponseEntity.notFound().build();
-            }
-
-            taskToUpdate.setDescription(task.getDescription());
-            taskToUpdate.setCompleted(task.isCompleted());
-
-            taskRepository.save(taskToUpdate);
-
+            taskService.updateTask(id, task);
             return ResponseEntity.noContent().build();
+        } catch (TaskNotFoundException e) {
+            return ResponseEntity.notFound().build();
         } catch (Exception e) {
             log.error("Erro ao atualizar uma tarefa", e);
             return ResponseEntity.internalServerError().build();
@@ -107,15 +95,10 @@ public class TaskController {
     @Operation(summary = "Deleta uma tarefa específica")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
         try {
-            Task task = taskRepository.findById(id).orElse(null);
-
-            if (task == null) {
-                return ResponseEntity.notFound().build();
-            }
-
-            taskRepository.delete(task);
-
+            taskService.deleteTask(id);
             return ResponseEntity.noContent().build();
+        } catch (TaskNotFoundException e) {
+            return ResponseEntity.notFound().build();
         } catch (Exception e) {
             log.error("Erro ao deletar uma tarefa", e);
             return ResponseEntity.internalServerError().build();
